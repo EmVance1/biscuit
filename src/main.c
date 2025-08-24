5c1cd45921386fdf542d90edf580237e31b56b3b
Em Vance <ayv200@gmail.com>
2025-08-24 16:20:39 +0200

    window boilerplate

--- src/main.c
+++ src/main.c
@@ -1,8 +1,36 @@
+#include <SFML/Graphics.h>
 #include <stdio.h>
 
 
+#ifdef VANGO_DEBUG
+    #define VIDEO_MODE (sfVideoMode){ .width=1920, .height=1080, .bitsPerPixel=32 }
+    #define SCREEN_MODE sfDefaultStyle
+#else
+    #define VIDEO_MODE sfVideoMode_isValid((sfVideoMode){ .width=1920, .height=1080, .bitsPerPixel=32 }) ? \
+        (sfVideoMode){ .width=1920, .height=1080, .bitsPerPixel=32 } : sfVideoMode_getDesktopMode()
+    #define SCREEN_MODE sfFullscreen
+#endif
+
+
 int main() {
-    
-    printf("Hello World!\n");
+    sfRenderWindow* window = sfRenderWindow_create(VIDEO_MODE, "Biscuit", SCREEN_MODE, NULL);
+
+    while (sfRenderWindow_isOpen(window)) {
+        sfEvent event;
+        while (sfWindow_pollEvent((sfWindow*)window, &event)) {
+            switch (event.type) {
+            case sfEvtClosed:
+                sfRenderWindow_close(window);
+                break;
+            default:
+                break;
+            }
+        }
+
+        sfRenderWindow_clear(window, (sfColor){ 255, 0, 255, 255 });
+        sfRenderWindow_display(window);
+    }
+
+    sfRenderWindow_destroy(window);
 }
 
