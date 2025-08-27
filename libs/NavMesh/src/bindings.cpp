fb0ac808845e8d67f4d74b0e102f0629321695b4
Em Vance <ayv200@gmail.com>
2025-08-27 15:52:18 +0200

    fixed vector method linker errors

--- libs/NavMesh/src/bindings.cpp
+++ libs/NavMesh/src/bindings.cpp
@@ -7,6 +7,34 @@ static nav::Vector2f fromc(navVector2f v) { return nav::Vector2f{ v.x, v.y }; }
 static navVector2f intoc(nav::Vector2f v) { return navVector2f{ v.x, v.y }; }
 
 
+navVector2f navVec2f_add(navVector2f a, navVector2f b) { return navVector2f{ a.x + b.x, a.y + b.y }; }
+navVector2f navVec2f_sub(navVector2f a, navVector2f b) { return navVector2f{ a.x - b.x, a.y - b.y }; }
+navVector2f navVec2f_scale(navVector2f a, float b) { return navVector2f{ a.x * b, a.y * b }; }
+float navVec2f_dot(navVector2f a, navVector2f b) { return a.x * b.x + a.y * b.y; }
+float navVec2f_len(navVector2f v) { return sqrtf(navVec2f_dot(v, v)); }
+navVector2f navVec2f_norm(navVector2f v) { return navVec2f_scale(v, 1.f / navVec2f_len(v)); }
+navVector2f navVec2f_compDiv(navVector2f a, navVector2f b) { return navVector2f{ a.x / b.x, a.y / b.y }; }
+navVector2f navVec2f_compMul(navVector2f a, navVector2f b) { return navVector2f{ a.x * b.x, a.y * b.y }; }
+navVector2f navVec2f_perpCw(navVector2f v)   { return navVector2f{ v.y, -v.x }; }
+navVector2f navVec2f_perpCcw(navVector2f v)  { return navVector2f{ -v.y, v.x }; }
+float navVec2f_cmp(navVector2f a, navVector2f b)   { const float c = a.x - b.x; const float d = a.y - b.y; return c > d ? c : d; }
+float navVec2f_angle(navVector2f a, navVector2f b) { const float c = navVec2f_dot(a, b); const float d = navVec2f_len(a) * navVec2f_len(b); return std::acos(c / d); }
+
+navVector2i navVec2i_add(navVector2i a, navVector2i b) { return navVector2i{ a.x + b.x, a.y + b.y }; }
+navVector2i navVec2i_sub(navVector2i a, navVector2i b) { return navVector2i{ a.x - b.x, a.y - b.y }; }
+navVector2f navVec2i_scale(navVector2i a, float b) { return navVector2f{ (float)a.x * b, (float)a.y * b }; }
+int   navVec2i_dot(navVector2i a, navVector2i b) { return a.x * b.x + a.y * b.y; }
+float navVec2i_len(navVector2i v) { return navVec2f_len(navVector2f{ (float)v.x, (float)v.y }); }
+navVector2f navVec2i_norm(navVector2i v) { return navVec2i_scale(v, 1.f / navVec2i_len(v)); }
+navVector2f navVec2i_compDiv(navVector2i a, navVector2i b) { return navVector2f{ (float)a.x / (float)b.x, (float)a.y / (float)b.y }; }
+navVector2i navVec2i_compDivFloor(navVector2i a, navVector2i b) { return navVector2i{ a.x / b.x, a.y / b.y }; }
+navVector2i navVec2i_compMul(navVector2i a, navVector2i b) { return navVector2i{ a.x * b.x, a.y * b.y }; }
+navVector2i navVec2i_perpCw(navVector2i v)   { return navVector2i{ v.y, -v.x }; }
+navVector2i navVec2i_perpCcw(navVector2i v)  { return navVector2i{ -v.y, v.x }; }
+int   navVec2i_cmp(navVector2i a, navVector2i b)   { const int c = a.x - b.x; const int d = a.y - b.y; return c > d ? c : d; }
+float navVec2i_angle(navVector2i a, navVector2i b) { const float c = (float)navVec2i_dot(a, b); const float d = navVec2i_len(a) * navVec2i_len(b); return std::acos(c / d); }
+
+
 bool navCircle_contains(const navCircle* self, navVector2f p) {
     return nav::FloatCircle{ fromc(self->pos), self->radius }.contains(fromc(p));
 }
