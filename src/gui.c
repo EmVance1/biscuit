3dc377c18a8ac58a597716f3da3ecf79a8a52963
Em Vance <ayv200@gmail.com>
2025-08-30 19:11:57 +0200

    ui textures

--- src/gui.c
+++ src/gui.c
@@ -1,5 +1,5 @@
 #include "gui.h"
-#include "SFML/Graphics/RenderWindow.h"
+#include <stdbool.h>
 #include <math.h>
 
 
@@ -10,6 +10,9 @@ static float hit_current;
 static float dash_current;
 static float fireball_current;
 
+static sfTexture* hit_tex;
+static sfTexture* dash_tex;
+static sfTexture* fireball_tex;
 
 void Gui_init(float _hit_max, float _dash_max, float _fireball_max) {
     hit_max = _hit_max;
@@ -18,6 +21,10 @@ void Gui_init(float _hit_max, float _dash_max, float _fireball_max) {
     hit_current = 0;
     dash_current = 0;
     fireball_current = 0;
+
+    hit_tex = sfTexture_createFromFile("res/textures/sword_ui.png", NULL);
+    dash_tex = sfTexture_createFromFile("res/textures/dash_ui.png", NULL);
+    fireball_tex = sfTexture_createFromFile("res/textures/fire_ui.png", NULL);
 }
 
 void Gui_update(float _hit_current, float _dash_current, float _fireball_current) {
@@ -32,11 +39,19 @@ void Gui_render(sfRenderWindow* window) {
     sfRectangleShape_setPosition(rect, (sfVector2f){ 50, 50 });
     sfRectangleShape_setSize(rect, (sfVector2f){ 100, 100 });
     sfRectangleShape_setFillColor(rect, (sfColor){ 255, 255, 255, 255 });
+    sfRectangleShape_setOutlineThickness(rect, 5.f);
+    sfRectangleShape_setOutlineColor(rect, (sfColor){ 0, 0, 0, 255 });
+    sfRectangleShape_setTexture(rect, hit_tex, true);
     sfRenderWindow_drawRectangleShape(window, rect, NULL);
     sfRectangleShape_setPosition(rect, (sfVector2f){ 200, 50 });
+    sfRectangleShape_setTexture(rect, dash_tex, true);
     sfRenderWindow_drawRectangleShape(window, rect, NULL);
     sfRectangleShape_setPosition(rect, (sfVector2f){ 350, 50 });
+    sfRectangleShape_setTexture(rect, fireball_tex, true);
     sfRenderWindow_drawRectangleShape(window, rect, NULL);
+    sfRectangleShape_setOutlineThickness(rect, 0.f);
+
+    sfRectangleShape_setTexture(rect, NULL, true);
 
     sfRectangleShape_setFillColor(rect, (sfColor){ 255, 0, 0, 100 });
     if (hit_current < INFINITY) {
